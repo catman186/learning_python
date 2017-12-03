154e9289ed2989b783089f3983e0de08292f2086
catman186 <rshillito@outlook.com>
2017-12-03 18:57:11 +1300

    Update glossary2.py

--- glossary2.py
+++ glossary2.py
@@ -1,9 +1,16 @@
 jobs = {
-    'Mary': 'Doctor',
-    'Billy': 'Hair-stylist',
-    'Margaret': 'Racecar Driver',
-    'Greg': 'Waiter',
-    'Susan': 'Engineer',
+    'mary': 'doctor',
+    'billy': 'hair-stylist',
+    'margaret': 'racecar driver',
+    'greg': 'waiter',
+    'susan': 'engineer',
     }
 
-print(
+jobs['peter'] = 'nurse'
+jobs['edna'] = 'estate agent'
+jobs['wilson'] = 'teacher'
+jobs['jacob'] = 'footballer'
+jobs['hillary'] = 'accountant'
+
+for name, job in jobs.items():
+    print(name.title() + " is a " + job.title())
